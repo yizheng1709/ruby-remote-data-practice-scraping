e7df95f36b3f757cd6697bbc759feaaee2fb630a
Eva <yi.zheng1709@gmail.com>
2021-01-27 20:25:37 -0500

    commit tree be82496f3613007f20f83846c02193713980fbcf

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4") 
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end 
+
+p doc.css(".tout__label.heading.heading--level-4")[0].attributes 
